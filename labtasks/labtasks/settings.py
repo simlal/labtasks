1699a574f6f1592bce7259e59afc3208488e5f32
simlal <simlalonde@hotmail.com>
2023-02-24 10:47:58 -0500

    Fix secretkey relative import

--- labtasks/labtasks/settings.py
+++ labtasks/labtasks/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from secretkey import dj_secret_key
+from .secretkey import dj_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
