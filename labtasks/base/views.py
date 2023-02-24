7564b92d40c2fc7566c9d377a4ce4ab37a4e751c
simlal <simlalonde@hotmail.com>
2023-02-24 13:52:25 -0500

    Initialize base and lab views

--- labtasks/base/views.py
+++ labtasks/base/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Test")
+    return render(request, "index.html")
+
+def lab(request):
+    return render(request, "lab.html")
--- labtasks/labtasks/settings.py
+++ labtasks/labtasks/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    "base.apps.BaseConfig"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,9 @@ ROOT_URLCONF = "labtasks.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates"    
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
