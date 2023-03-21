cdccd6cadff9702a0b1c51f5e33d37d26629bb13
lals2906 <lals2906@dinf-tairee-11i.DInf.fsci.usherbrooke.ca>
2023-03-21 14:24:00 -0400

    Implement sendgrid email api on validated contact_form

--- labtasks/base/views.py
+++ labtasks/base/views.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 from contact_email.models import Contact
 from contact_email.forms import ContactForm
 
+from django.core.mail import send_mail, BadHeaderError
+from django.conf import settings
+
 
 
 def returned_message(first_name):
@@ -12,11 +15,11 @@ def returned_message(first_name):
         Thank you for contacting us at 🧪 LaboraTasks 🥼. Our team will get back to you ASAP.
 
         In the meantime, stay safe!
-        \x1B[1;3mThe LaboraTask team\x1B[0;3m
+        𝙏𝙝𝙚 𝙇𝙖𝙗𝙤𝙧𝙖𝙏𝙖𝙨𝙠𝙨 𝙏𝙚𝙖𝙢
         """
+    return message
 
 def index(request):
-    
     submitted=False
     # Form validation and saving to db with the associated model
     if request.method == "POST":
@@ -25,10 +28,30 @@ def index(request):
             cd = contact.cleaned_data
             if cd["last_name"] == "":    # Changing not-required field to NULL val
                 cd["last_name"] = None
-            contact_instance = contact.save(commit=False)   # To add the submit date but not using the form fields
+            # Adding submit date (save to db) but not in form
+            contact_instance = contact.save(commit=False)
             contact_instance.send_date = timezone.now()
             contact_instance.save()
             submitted = True
+            context = {
+                "form" : contact,
+                "submitted" : submitted
+            }
+            # Sending confirmation email
+            form_vals = "\n".join(cd.values())
+            try:
+                send_mail(
+                    subject=f"LaboraTasks confirmation: '{cd['subject']}'",
+                    message=returned_message(cd["first_name"]),
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    recipient_list=[cd["email"]]
+                )
+                print(form_vals)
+                print(returned_message(cd["first_name"]))
+            except BadHeaderError:
+                return render(request, "contact_email/header_error.html")
+
+            return render(request, "base/index.html", context=context)
     
     # To render contact form
     else :
@@ -40,7 +63,7 @@ def index(request):
         "submitted": submitted,
     }
     # print(context)
-    return render(request, "base/index.html", context)
+    return render(request, "base/index.html", context=context)
 
 def spaces(request, pk):
     return render(request, "base/spaces.html")
