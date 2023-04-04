8a54a7feb35eb560adc4d62e3b499335f73a904a
lals2906 <lals2906@dinf-tairee-11i.DInf.fsci.usherbrooke.ca>
2023-04-04 08:56:17 -0400

    Add contact email db to admin

--- labtasks/contact_email/models.py
+++ labtasks/contact_email/models.py
@@ -7,3 +7,6 @@ class Contact(models.Model):
     subject = models.CharField(max_length=255)
     message = models.CharField(max_length=2000)
     send_date = models.DateField()
+
+    def __str__(self):
+        return f"Message from {self.email} (sent on {self.send_date})"
